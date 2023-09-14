e5d701e01cc778171f2f28affa68a0d78a4526ca
Zorin Mikhail <zorin.mikhail.84284@gmail.com>
2023-09-14 16:42:59 +0300

    ♻️ HW04 refacrored

---
+++ HW04/ConsoleInput.cs
@@ -0,0 +1,48 @@
+﻿namespace HW04
+{
+    public static class ConsoleInput
+    {
+        private static String? GetConsoleInput(String inputPrompt)
+        {
+            Console.Write(inputPrompt);
+            return Console.ReadLine();
+        }
+
+        public static Boolean Take(String inputPrompt, out String result)
+        {
+            String? consoleInput = GetConsoleInput(inputPrompt);
+            if (consoleInput != null)
+            {
+                result = consoleInput;
+                return true;
+            }
+            result = String.Empty;
+            return false;
+        }
+
+        public static Boolean Take(String inputPrompt, out Int32? result)
+        {
+            String? consoleInput = GetConsoleInput(inputPrompt);
+            if (Int32.TryParse(consoleInput, out Int32 inputValue))
+            {
+                result = inputValue;
+                return true;
+            }
+            result = null;
+            return false;
+        }
+
+        public static Boolean Take(String inputPrompt, out EGender? result)
+        {
+            String? consoleInput = GetConsoleInput(inputPrompt);
+            if (Int32.TryParse(consoleInput, out Int32 inputValue))
+            {
+                EGender? gender = inputValue == 0 ? EGender.Female : inputValue == 1 ? EGender.Male : null;
+                result = gender;
+                return result != null;
+            }
+            result = null;
+            return false;
+        }
+    }
+}
--- HW04/Program.cs
+++ HW04/Program.cs
@@ -7,24 +7,13 @@ namespace HW04
         static void Main(string[] args)
         {
             Console.OutputEncoding = Encoding.UTF8;
+            
             Console.WriteLine("✨ Створення нового користувача. Задайте необхідні параметри:");
-            //
-            Console.Write(" • Стать (0 - жіноча, 1 - чоловіча, інше - невизначено): ");
-            Int32 inputGender;
-            EGender? gender = null;
-            if (Int32.TryParse(Console.ReadLine(), out inputGender)) 
-                gender = inputGender == 0 ? EGender.Female : inputGender == 1 ? EGender.Male : null;
-            //
-            Console.Write(" • Вік: ");
-            Int32 inputAge;
-            Int32? age = Int32.TryParse(Console.ReadLine(), out inputAge) ? inputAge : null;
-            //
-            Console.Write(" • Ім'я: ");
-            String firstName = Console.ReadLine() ?? String.Empty;
-            //
-            Console.Write(" • Прізвище: ");
-            String lastName = Console.ReadLine() ?? String.Empty;
-            //
+            ConsoleInput.Take(" • Стать (0 - жіноча, 1 - чоловіча, інше - невизначено): ", out EGender? gender);
+            ConsoleInput.Take(" • Вік: ", out Int32? age);
+            ConsoleInput.Take(" • Ім'я: ", out String firstName);
+            ConsoleInput.Take(" • Прізвище: ", out String lastName);
+            
             User newUser = new ()
             {
                 Gender = gender,
